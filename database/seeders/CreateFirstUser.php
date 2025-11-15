bce3bca29d7732d851628275cd384c99e6b60b7c
AnnisaPutriAnanda24si <annisa24si@mahasiswa.pcr.ac.id>
2025-11-15 17:03:44 +0700

    seeder

--- database/seeders/CreateFirstUser.php
+++ database/seeders/CreateFirstUser.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CreateFirstUser extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+         User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password123'),
+        ]);
     }
 }
--- database/seeders/CreateWargaPersil.php
+++ database/seeders/CreateWargaPersil.php
@@ -2,16 +2,36 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Warga;
+use App\Models\Persil;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CreateWargaPersil extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+         $warga = Warga::create([
+            'no_ktp' => '1234567890123456',
+            'nama' => 'Nurtanio Luqman',
+            'jenis_kelamin' => 'Male',
+            'agama' => 'Islam',
+            'pekerjaan' => 'Karyawan Swasta',
+            'email' => 'luqman@email.com',
+            'telp' => '081234567890',
+        ]);
+
+        Persil::create([
+            'kode_persil' => 'P001',
+            'pemilik_warga_id' => $warga->warga_id,
+            'luas_m2' => 150,
+            'penggunaan' => 'Perumahan',
+            'alamat_lahan' => 'Jl. Merdeka No.1',
+            'rt' => '01',
+            'rw' => '02',
+        ]);
     }
 }
